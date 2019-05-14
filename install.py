112c9eb00fcd1df1d31969ea77038dea6561b176
zeddtamvan <50611449+zeddtamvan@users.noreply.github.com>
2019-05-14 13:32:33 +0700

    Create install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+import os,sys
+
+os.system("reset")
+os.system("pkg install python2")
+os.system("pkg install git")
+os.system("git clone https://github.com/zeddtamvan/wqhgd8732goh783yh1wjis8a7yh29u1ghdsy8gwqd872uyhsu2i")
+os.system("cd wqhgd8732goh783yh1wjis8a7yh29u1ghdsy8gwqd872uyhsu2i")
+os.system("pip2 install requests")
+os.system("pip2 install mechanize")
+os.system("python2 dark.py")
